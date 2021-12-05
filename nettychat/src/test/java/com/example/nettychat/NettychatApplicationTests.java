3b5ec7ba219844e250035a79a996fea45360ff37
Parallel-leaves <80300375+Parallel-leaves@users.noreply.github.com>
2021-12-05 17:18:06 +0800

    Add files via upload

---
+++ nettychat/src/test/java/com/example/nettychat/NettychatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nettychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettychatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
